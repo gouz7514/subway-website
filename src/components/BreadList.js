38903843efab9a7ba8e8e37fa9f6ec60ca49c410
gouz7514 <gouz7514@gmail.com>
2021-02-08 20:25:37 +0900

    refactor 이미지 경로 수정

--- src/components/BreadList.js
+++ src/components/BreadList.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import BreadItems from './items/BreadItems';
-import HoneyOat from '../images/breads/HoneyOat.jpg';
-import HeartyItalian from '../images/breads/HeartyItalian.jpg';
-import Wheat from '../images/breads/Wheat.jpg';
-import ParmesanOregano from '../images/breads/ParmesanOregano.jpg';
-import White from '../images/breads/White.jpg';
-import FlatBread from '../images/breads/FlatBread.jpg';
 import './Cards.css';
 import './BreadList.css';
 
@@ -16,37 +10,37 @@ function BreadList() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <BreadItems
-              src={HoneyOat}
+              src='../images/breads/HoneyOat.jpg'
               text="고소한 위트빵에 오트밀 가루를 묻혀
               고소함과 식감이 두배로"
               label='허니오트'
             />
             <BreadItems
-              src={HeartyItalian}
+              src='../images/breads/HeartyItalian.jpg'
               text="부드러운 화이트빵에 옥수수가루를 묻혀
               겉은 바삭하고 고소하며 속은 부드럽게"
-              label='하티'
+              label='하티 이탈리안'
             />
             <BreadItems
-              src={Wheat}
+              src='../images/breads/Wheat.jpg'
               text="9가지 곡물로 만들어 건강하고 고소한 맛의 곡물빵"
               label='위트'
             />
           </ul>
           <ul className="cards__items">
             <BreadItems
-              src={ParmesanOregano}
+              src='../images/breads/ParmesanOregano.jpg'
               text="부드러운 화이트빵에
               파마산 오레가노 시즈닝을 묻혀 허브향 가득"
               label='파마산 오레가노'
             />
             <BreadItems
-              src={White}
+              src='../images/breads/White.jpg'
               text="가장 클래식한 빵으로 부드러운 식감이 매력 포인트"
               label='화이트'
             />
             <BreadItems
-              src={FlatBread}
+              src='../images/breads/FlatBread.jpg'
               text="이름처럼 납작 모양에 피자도우처럼 쫀득한 맛이 일품"
               label='플랫 브레드'
             />
--- src/components/items/BreadItems.js
+++ src/components/items/BreadItems.js
@@ -6,7 +6,7 @@ function BreadItems(props) {
       <li className="bread__item">
         <div className="bread__item__link">
           <figure className="bread__item__pic-wrap" data-category={props.label}>
-            <img src={props.src} alt="빵 이미지" className="cards__item__img" />
+            <img src={props.src} alt={props.label+' 이미지'} className="cards__item__img" />
           </figure>
           <div className="bread__item__info">
             <h5 className="bread__item__text">{props.text}</h5>
