487209d7185fc3bef9e7c58ada6d75bd72a8e26e
gouz7514 <gouz7514@gmail.com>
2021-02-17 17:54:37 +0900

    style : css 수정

--- src/components/SliderSection.js
+++ src/components/SliderSection.js
@@ -19,12 +19,24 @@ function SliderSection() {
       pagination
       // onSwiper={(swiper) => console.log(swiper)}
     > 
-      <SwiperSlide>시작해보세요</SwiperSlide>
-      <SwiperSlide>STEP 1 메뉴 선택</SwiperSlide>
-      <SwiperSlide>STEP 2 빵 선택</SwiperSlide>
-      <SwiperSlide>STEP 3 추가토핑 선택</SwiperSlide>
-      <SwiperSlide>STEP 4 야채&소스 선택</SwiperSlide>
-      <SwiperSlide>STEP 5 세트 선택</SwiperSlide>
+      <SwiperSlide>
+          시작해보세요
+      </SwiperSlide>
+      <SwiperSlide>
+        STEP 1 메뉴 선택
+      </SwiperSlide>
+      <SwiperSlide>
+        STEP 2 빵 선택
+        </SwiperSlide>
+      <SwiperSlide>
+        STEP 3 추가토핑 선택
+      </SwiperSlide>
+      <SwiperSlide>
+        STEP 4 야채&소스 선택
+      </SwiperSlide>
+      <SwiperSlide>
+        STEP 5 세트 선택
+      </SwiperSlide>
       ...
     </Swiper>
     </React.Fragment>
