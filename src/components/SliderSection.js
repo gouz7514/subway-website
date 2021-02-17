caf45baaf4625bbf1910472766cfb54c24546777
gouz7514 <gouz7514@gmail.com>
2021-02-17 18:38:40 +0900

    create : Slider 내부 채 시작

--- src/components/SliderSection.js
+++ src/components/SliderSection.js
@@ -20,22 +20,89 @@ function SliderSection() {
       // onSwiper={(swiper) => console.log(swiper)}
     > 
       <SwiperSlide>
-          시작해보세요
+        <div className="swiper-slide-content-intro">
+      써브웨이는 다양한 메뉴와 재료를 제공하지만<br></br>
+      주문 시 번거로움으로 인해 망설이는 사람들이 많습니다.<br></br>
+      어케했Sub과 함께 써브웨이를 즐겨보세요!
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        STEP 1 메뉴 선택
+        <div className="swiper-slide-content">
+          <div className="title-wrapper">
+            <h3 className="title-text">
+            STEP 1 메뉴 선택
+            </h3>
+          </div>
+          <div className="detail-wrapper">
+            <div className="detail-text">
+              다양한 메뉴 중 원하는 메뉴를 고르세요.<br></br>
+              15cm, 30cm 중 선택이 가능합니다.<br></br>
+              자세한 메뉴 설명은 아래 버튼을 클릭하세요!<br></br>
+              <button className="menu-view-button">메뉴 보기</button>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        STEP 2 빵 선택
+        <div className="swiper-slide-content">
+          <div className="title-wrapper">
+            <h3 className="title-text">
+            STEP 2 빵 선택
+            </h3>
+          </div>
+          <div className="detail-wrapper">
+            <span className="detail-text">
+              6가지 종류 중 원하는 빵으로<br></br>
+              무엇이든 선택하세요.<br></br>
+            </span>
+          </div>
+        </div>
         </SwiperSlide>
       <SwiperSlide>
-        STEP 3 추가토핑 선택
+      <div className="swiper-slide-content">
+          <div className="title-wrapper">
+            <h3 className="title-text">
+            STEP 3 추가토핑 선택
+            </h3>
+          </div>
+          <div className="detail-wrapper">
+            <span className="detail-text">
+              베이컨, 새우 등 다양한 토핑을 추가해<br></br>
+              나만의 써브웨이를 즐기세요!<br></br>
+            </span>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        STEP 4 야채&소스 선택
+      <div className="swiper-slide-content">
+          <div className="title-wrapper">
+            <h3 className="title-text">
+            STEP 4 야채&소스 선택
+            </h3>
+          </div>
+          <div className="detail-wrapper">
+            <span className="detail-text">
+              싫어하는 야채는 드실 필요 없습니다!<br></br>
+              좋아하는 야채는 마음껏 드세요.<br></br>
+              소스는 무엇이든 선택해 드셔 보세요!<br></br>
+            </span>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        STEP 5 세트 선택
+      <div className="swiper-slide-content">
+          <div className="title-wrapper">
+            <h3 className="title-text">
+            STEP 5 세트 선택
+            </h3>
+          </div>
+          <div className="detail-wrapper">
+            <span className="detail-text">
+              써브웨이의 별미!<br></br>
+              쿠키, 칩과 함께 즐겨보세요!<br></br>
+            </span>
+          </div>
+        </div>
       </SwiperSlide>
       ...
     </Swiper>
