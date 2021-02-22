a350a0ecdc699bdaa4e02c0fc2bb9da9c969c012
gouz7514 <gouz7514@gmail.com>
2021-02-22 19:00:04 +0900

    style : 칼로리 추가 및 스타일 수정

--- src/components/items/MenuItems.js
+++ src/components/items/MenuItems.js
@@ -7,6 +7,7 @@ function MenuItems(props) {
         <div className="ingredient__item__link large">
           <figure className="ingredient__item__pic-wrap" data-category={props.label}>
             <img src={props.src+'?tr=w-400,h-300,bl-30,q-50'} alt={props.label+' 이미지'} className="cards__item__img" data-lazy={props.src}/>
+            <span className="ingredient__item__label">{props.calorie}</span>
           </figure>
           <div className="ingredient__item__info large">
             <h5 className="ingredient__item__text">{props.text.replaceAll('<br>', '\n')}</h5>
