cb9754dce1b51a31b1e4b7ec446e376f14f993b3
gouz7514 <gouz7514@gmail.com>
2021-02-22 19:22:36 +0900

    style : 반응형 수정

--- src/components/items/MenuItems.js
+++ src/components/items/MenuItems.js
@@ -4,7 +4,7 @@ function MenuItems(props) {
   return (
     <>
       <li className="ingredient__item">
-        <div className="ingredient__item__link large">
+        <div className="ingredient__item__link">
           <figure className="ingredient__item__pic-wrap" data-category={props.label}>
             <img src={props.src+'?tr=w-400,h-300,bl-30,q-50'} alt={props.label+' 이미지'} className="cards__item__img" data-lazy={props.src}/>
             <span className="ingredient__item__label">{props.calorie}</span>
